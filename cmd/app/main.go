0c4d6dc5741354a86a5b9ce24f5d0791e8335384
stpotter16 <stpotter1642@gmail.com>
2025-02-11 10:16:42 -0500

    Read port configuration from environment

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"net/http"
+	"os"
 	"os/signal"
 	"syscall"
 
@@ -11,20 +12,29 @@ import (
 )
 
 func main() {
+    port := loadPortConfiguration()
     s := handlers.New()
-    if err := run(s); err != nil {
+    if err := run(port, s); err != nil {
         log.Fatalf("Application run failed: %v", err)
     }
 }
 
-func run(s handlers.Server) error {
+func run(port string, s handlers.Server) error {
     ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGTERM)
     defer stop()
 
-    http.ListenAndServe(":8080", s.Handler())
+    http.ListenAndServe(port, s.Handler())
 
     <-ctx.Done()
     log.Print("Received termination signal. Shutting down")
 
     return nil
+}
+
+func loadPortConfiguration() string {
+    port, present := os.LookupEnv("PORT")
+    if !present {
+        log.Fatalf("Unable to find $PORT environment variable")
+    }
+    return port
 }
